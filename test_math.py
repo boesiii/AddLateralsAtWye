f4805270d3e702527917892ba43ef08277b4daf4
boesiii <boesiii@yahoo.com>
2019-11-21 16:44:26 -0500

    Add files via upload

--- test_math.py
+++ test_math.py
@@ -1,20 +1,21 @@
 import math
 
-start_x = 1291764.86
-start_y = 474261.69
-final_x = 1291772.78
-final_y = 474267.79
+start_x = 1307796.60
+start_y = 420858.24      
+final_x = 1307801.75
+final_y = 420866.81
 
-c = '01:00'
-a = 142.42
+a = 121
 print (a)
 a = a - 90
 print (a)
 length = 10
 
 #calculate new position using length
-endx = length * math.cos(a)
-endy = length * math.sin(a)
+endx = length * math.cos(math.radians(a))
+endy = length * math.sin(math.radians(a))
+print (endx)
+print (endy)
 new_x = start_x + endx
 new_y = start_y + endy
 print ('final_x',' ' , final_x)
